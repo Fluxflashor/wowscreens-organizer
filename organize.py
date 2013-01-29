a58f26c77201f0e39452c6ac51f160bb8c24316e
Robert Veitch <Robert@Fluxflashor.net>
2013-01-29 11:42:53 -0500

    expansion start dates. i have no idea how im doing this

---
+++ organize.py
@@ -0,0 +1,10 @@
+import os
+
+# WoWScrnShot_MMDDYY_TTTTTT.EXT
+wow_expansion_dates = {
+    '1x': '112304', # November 23, 2004
+    '2x': '011607', # January 16, 2007
+    '3x': '111308', # November 13, 2008
+    '4x': '120710', # December 7, 2010
+    '5x': '082512'  # September 25, 2012
+}
