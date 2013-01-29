e0b7a04302101432154e0a279def35930b736154
Robert Veitch <Robert@Fluxflashor.net>
2013-01-29 12:02:39 -0500

    fixed error with expansion dates and importing re.. oh god why regex why

--- organize.py
+++ organize.py
@@ -1,8 +1,9 @@
 import os
+import re
 
 # WoWScrnShot_MMDDYY_TTTTTT.EXT
 wow_expansion_dates = {
-    '0x-warcraft.beta': '010100'           # January 1, 2000 (safety)
+    '0x-warcraft.beta': '010100',          # January 1, 2000 (safety)
     '1x-classic': '112304',                # November 23, 2004
     '2x-the.burning.crusade': '011607',    # January 16, 2007
     '3x-wrath.of.the.lich.king': '111308', # November 13, 2008
