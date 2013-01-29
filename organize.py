b7e6412440e5836644dab8cc8afbff8b0a00b120
Robert Veitch <Robert@Fluxflashor.net>
2013-01-29 13:43:05 -0500

    changing our datestamp format to YYMMDD in expansion dates from MMDDYY

--- organize.py
+++ organize.py
@@ -7,33 +7,33 @@ import shutil
 wow_expansion_dates = {
     '0x': {            # January 1, 2000 (safety)
         'folder-name': 'wow-beta',
-              'start': '010100',
-                'end': '112204'
+              'start': '000101',
+                'end': '041122'
     },         
     '1x': {            # November 23, 2004
         'folder-name': 'classic',
-              'start': '112304',
-                'end': '011507'
+              'start': '041123',
+                'end': '070115'
     },
     '2x': {            # January 16, 2007
         'folder-name': 'the-burning-crusade',
-              'start': '011607',
-                'end': '111208'
+              'start': '070116',
+                'end': '081112'
     },
     '3x': {            # November 13, 2008
         'folder-name': 'wrath-of-the-lich-king',
-              'start': '111308',
-                'end': '120610'
+              'start': '081113',
+                'end': '101206'
     },
     '4x': {            # December 7, 2010
         'folder-name': 'cataclysm',
-              'start': '120710',
-                'end': '082412'
+              'start': '101207',
+                'end': '120824'
     },
     '5x': {            # September 25, 2012
         'folder-name': 'mists-of-pandaria',
-              'start': '082512',
-                'end': '010115'
+              'start': '120825',
+                'end': '150101'
     }
 }
 
