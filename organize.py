1cc71203fa8f9ebbf3013653f38c65e32f7644eb
Robert Veitch <Robert@Fluxflashor.net>
2013-01-29 11:45:37 -0500

    wowbeta added to the list. also changed up the folder names

--- organize.py
+++ organize.py
@@ -2,9 +2,10 @@ import os
 
 # WoWScrnShot_MMDDYY_TTTTTT.EXT
 wow_expansion_dates = {
-    '1x': '112304', # November 23, 2004
-    '2x': '011607', # January 16, 2007
-    '3x': '111308', # November 13, 2008
-    '4x': '120710', # December 7, 2010
-    '5x': '082512'  # September 25, 2012
+    '0x-warcraft.beta': '010100'           # January 1, 2000 (safety)
+    '1x-classic': '112304',                # November 23, 2004
+    '2x-the.burning.crusade': '011607',    # January 16, 2007
+    '3x-wrath.of.the.lich.king': '111308', # November 13, 2008
+    '4x-cataclysm': '120710',              # December 7, 2010
+    '5x-mists.of.pandaria': '082512'       # September 25, 2012
 }
